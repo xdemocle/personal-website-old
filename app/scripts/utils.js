41d0b1b61486d3da692bfc3b04cc2ed8c65c9bd4
Rocco <xdemocle@gmail.com>
2015-06-12 03:27:42 +0200

    adding more utils functions and update the modal windows

--- app/scripts/utils.js
+++ app/scripts/utils.js
@@ -1,13 +1,20 @@
 /* jshint devel:true */
-/** * Utilities Rocco.me JavaScript file */
+/**
+ * Utilities Rocco.me JavaScript file 
+ */
 (function(win) {
+
   'use strict';
+
   var doc = win.document;
 
   /**
-   * [callAjax description]
+   * [ajax description]
+   * @param  {[type]}   url      [description]
+   * @param  {Function} callback [description]
+   * @return {[type]}            [description]
    */
-  var callAjax = function(url, callback) {
+  var ajax = function(url, callback) {
     if (typeof XMLHttpRequest === 'undefined') {
       return;
     }
@@ -24,59 +31,184 @@
     xmlhttp.send();
   };
 
+  // Create element
+  var createEl = function(tag) {
+    return doc.createElement(tag);
+  };
+
   /**
    * Very simple modal
+   * @param  {[type]} msg  [description]
+   * @param  {[type]} type [description]
+   * @return {[type]}      [description]
    */
   var simpleModal = function(msg, type) { 
 
     // Select default status for type
     type = type || 'alert';
     
-    // Create element
-    function createEl(tag) {
-      return doc.createElement(tag);
-    }
+    // Local vars 
+    var modal = createEl('div'),
+      wrapper = createEl('div'),
+      content = createEl('div'),
+      text = createEl('div'),
+      confirmBox = createEl('div'),
+      className = 'modal ' + type;
     
-    // Create an modal div element
-    function createModalEl(htmlString) {
-      
-      // Local vars 
-      var modal = createEl('div'),
-        text = createEl('div'),
-        p = createEl('p'),
-        confirmBox = createEl('div'),
-        className = 'modal ' + type;
-      
-      // Set modal's class
-      modal.setAttribute('class', className);
-      text.setAttribute('class', 'text');
-      
-      // Set p content and append to text
-      p.innerHTML = htmlString;
-      text.appendChild(p);
-      
-      // Append text to
-      modal.appendChild(text);
-      
-      // Add confirm html
-      if (type === 'confirm') {
-        confirmBox.setAttribute('class', 'confirmBox');
-        confirmBox.innerHTML = '<div><a href="#">Okay</a><a href="#">Nope</a></div>';
-        modal.appendChild(confirmBox);
-      }
+    // Set modal's classes
+    addClass(modal, className);
+    addClass(wrapper, 'modal-wrapper');
+    addClass(content, 'modal-content');
+    addClass(text, 'text');
+    
+    // Set p content and append to text
+    text.innerHTML = '<p class="firstLine">'+msg.firstLine+'</p>' +
+      '<p class="secondLine">'+msg.secondLine+'</p>';
+    
+    // Append text to
+    content.appendChild(text);
+    wrapper.appendChild(content);
 
-      console.log(modal);
-      
-      // Append to body of page
-      doc.body.appendChild(modal); 
+    // Append wrapper to
+    modal.appendChild(wrapper);
+    
+    // Add confirm html
+    if (type === 'confirm') {
+
+      // Add class and other DOM manipulation
+      addClass(confirmBox, 'confirmBox');
+      confirmBox.innerHTML = '<div><a href="#okay">Show All Words</a>' +
+                             '<a href="#nope">Hide Bad Words</a></div>';
+
+      content.appendChild(confirmBox);
+
+      // All links in confirm box
+      var confirmLinks = confirmBox.getElementsByTagName('a');
+
+      // Event listening
+      listenEvent(confirmLinks, 'click', function(evt){
+
+        var hash = evt.target.hash.replace('#', '');
+
+        if (hash.length > 0) {
+
+          if (hash === 'nope') { addClass(doc.body, 'hide-harsh'); }
+          if (hash === 'okay') { removeClass(doc.body, 'hide-harsh'); }
+
+          removeClass(modal, 'open');
+          addClass(modal, 'close');
+
+          removeClass(doc.getElementById('main'), 'blur');
+        }
+
+        // Prevent normal behaviour
+        evt.preventDefault();
+      });
     }
 
-    // Create modal
-    createModalEl(msg);
+    // Add blur to #main
+    addClass(doc.getElementById('main'), 'blur');
+
+    // Append to body of page
+    doc.body.appendChild(modal);
+
+    // Add show the #main div
+    setTimeout(function(){
+      addClass(modal, 'open');
+    }, 100)
   };
 
-  // Register functions in window object
-  win.callAjax = callAjax;
-  win.simpleModal = simpleModal;
+  /**
+   * [addClass description]
+   * @param {[type]} element    [description]
+   * @param {[type]} classToAdd [description]
+   */
+  var addClass = function(element, classToAdd) {
+    if (!element) { return; }
+    var currentClassValue = element.className;
+     
+    if (currentClassValue.indexOf(classToAdd) === -1) {
+      if ((currentClassValue === null) || (currentClassValue === '')) {
+        element.className = classToAdd;
+      } else {
+        element.className += ' ' + classToAdd;
+      }
+    }
+
+    return element;
+  };
+
+  /**
+   * [removeClass description]
+   * @param  {[type]} element       [description]
+   * @param  {[type]} classToRemove [description]
+   * @return {[type]}               [description]
+   */
+  var removeClass = function(element, classToRemove) {
+    var currentClassValue = element.className;
+     
+    // removing a class value when there is more than one class value present
+    // and the class you want to remove is not the first one
+    if (currentClassValue.indexOf(' ' + classToRemove) !== -1) {
+      element.className = element.className.replace(' ' + classToRemove, '');
+      return;
+    }
+     
+    // removing the first class value when there is more than one class
+    // value present
+    if (currentClassValue.indexOf(classToRemove + ' ') !== -1) {
+      element.className = element.className.replace(classToRemove + ' ', '');
+      return;
+    }
+     
+    // removing the first class value when there is only one class value 
+    // present
+    if (currentClassValue.indexOf(classToRemove) !== -1) {
+      element.className = element.className.replace(classToRemove, '');
+      return;
+    }
+
+    return element;
+  };
+
+  /**
+   * [listenEvent description]
+   * @param  {[type]} els  [description]
+   * @param  {[type]} evt  [description]
+   * @param  {[type]} func [description]
+   * @return {[type]}      [description]
+   */
+  var listenEvent = function(els, evt, func) {
+
+    var listen = function(el) {
+
+      if (win.addEventListener) {
+        el.addEventListener(evt, func, false);
+      } else if (win.attachEvent) {
+        el.attachEvent(evt, func, false);
+      }      
+    };
+
+    if (typeof els === 'object' && els.length > 0) {
+      for (var i=0; i < els.length; i++) {
+        listen(els[i]);
+      }
+      return;
+    }
+
+    listen(els);
+  };
+
+  /**
+   * Register functions in window object
+   * @type {Object}
+   */
+  win.utils = {
+    'ajax': ajax,
+    'listenEvent': listenEvent,
+    'addClass': addClass,
+    'removeClass': removeClass,
+    'simpleModal': simpleModal
+  };
 
 })(this);
